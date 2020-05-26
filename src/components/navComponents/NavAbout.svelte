317c9dc979e7ec4515d14489bbcb1cf524384fdd
claxx moldii <jontrillana@gmail.com>
2020-05-26 08:54:20 +0800

    about page is okay. next is to fix nav as about page is scrolled down. also apply scroll effect as link is clicked. good commit...

---
+++ src/components/navComponents/NavAbout.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let segment;
+</script>
+
+<style type="text/css">
+  nav {
+    width: 100vw;
+    padding: 30px 50px;
+  }
+
+  h2 {
+    font-weight: 800;
+  }
+
+  a {
+    font-size: .9rem;
+    text-decoration: none;
+    margin-right: 25px;
+    padding-bottom: 5px;
+  }
+
+  a:hover {
+    color: darkgray;
+    cursor: pointer;
+    border-bottom: solid 1px #ff1d1d;
+  }
+
+  [aria-current] {
+    font-weight: 600;
+    border-bottom: solid 1px #ff1d1d;
+  }
+</style>
+
+{#if segment}
+<nav>
+  <h2 class="title siteColor">about</h2>
+  <a  aria-current='{segment === "about" ? "page" : undefined}' href='about'>partners</a>
+  <a  aria-current='{segment === "team" ? "page" : undefined}' href='team'>team</a>
+  <a  aria-current='{segment === "careers" ? "page" : undefined}' href='careers'>careers</a>
+  <a  aria-current='{segment === "contact" ? "page" : undefined}' href='contact'>contact</a>
+</nav>
+{/if}
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,70 @@
+<script type="text/javascript">
+  import NavAbout from '../components/navComponents/NavAbout.svelte';
+  import AboutPartners from '../components/aboutComponents/AboutPartners.svelte';
+  import AboutTeam from '../components/aboutComponents/AboutTeam.svelte';
+  import AboutCareers from '../components/aboutComponents/AboutCareers.svelte';
+  import AboutContact from '../components/aboutComponents/AboutContact.svelte';
+</script>
+
+<style type="text/css">
+  section {
+    max-width: calc(100vw-150px);
+    padding-bottom: 50px;
+    overflow-x: hidden;
+  }
+
+  nav {
+    position: fixed;
+    background-color: #fff;
+    z-index: 100;
+  }
+
+  main {
+    min-height: 100vh;
+    padding-top: 150px;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: [partners] auto [team] auto [careers] auto [contact] auto;
+  }
+
+  .aboutPartners {
+    grid-row: partners;
+  }
+
+  .aboutTeam {
+    grid-row: team;
+  }
+
+  .aboutCareers {
+    grid-row: careers;
+  }
+
+  .aboutContact {
+    grid-row: contact;
+  }
+</style>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<section>
+  <nav>
+    <NavAbout segment={'about'} />
+  </nav>
+  <main>
+    <div id="partners" class="aboutPartners">
+      <AboutPartners />
+    </div>
+    <div id="team" class="aboutTeam">
+      <AboutTeam />
+    </div>
+    <div id="careers" class="aboutCareers">
+      <AboutCareers />
+    </div>
+    <div id="contact" class="aboutContact">
+      <AboutContact />
+    </div>
+  </main>
+</section>
 
-<p>This is the 'about' page. There's not much here.</p>
